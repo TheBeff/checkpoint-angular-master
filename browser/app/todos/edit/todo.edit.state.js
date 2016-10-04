6739e14e7d4c191dd7cd5a73534ec47039162964
TheBeff <thebeff@gmail.com>
2016-10-04 19:41:51 -0400

    Finished Todos list

--- browser/app/todos/edit/todo.edit.state.js
+++ browser/app/todos/edit/todo.edit.state.js
@@ -2,3 +2,13 @@
 // based on the id in the url used to reach the state.
 // Note: it isn't tested, but you should also wire up the
 // associated controller and templateUrl for a functioning app.
+
+app.config(function($stateProvider){
+
+	$stateProvider
+	  .state('todos.edit', {
+	  	url:'/:id/edit',
+	  	templateUrl: './todo.edit.html'
+	  });
+
+})
--- browser/app/todos/list/todo.list.controller.js
+++ browser/app/todos/list/todo.list.controller.js
@@ -6,3 +6,36 @@
 // and a method `addTodo` that saves `$scope.toAdd` to the backend,
 // then goes to that new todo's edit state. (If you are confused about
 // where `$scope.toAdd` is coming from, check out the `todo.list.html`.)
+
+app.controller('TodoListCtrl', function($scope, todos, Todo, $state){
+	
+	$scope.todos = todos;
+
+	$scope.setCategory = function(category){
+		$scope.category = category;
+
+		if(category === "all")
+			$scope.filterByCompleted = '';
+		if(category === "completed")
+			$scope.filterByCompleted = true;
+		if(category === "active")
+			$scope.filterByCompleted = false;
+		if(category === "all")
+			$scope.filterByCompleted = '';
+	};
+
+	$scope.isActiveCategory = function(category){
+		if (category === $scope.category)
+			{return true;}
+		else
+			{return false;}
+	};
+
+	$scope.addTodo = function(todo){
+		Todo.add(todo)
+		  .then(function(todo){
+		  	$state.go('todos.edit', {id: todo.id});
+		  });
+	};
+
+});
