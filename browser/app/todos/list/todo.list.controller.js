33213c507752ae652a2772e24231c247c68c97d5
TheBeff <thebeff@gmail.com>
2016-10-04 18:40:59 -0400

    Initial commit

---
+++ browser/app/todos/list/todo.list.controller.js
@@ -0,0 +1,8 @@
+// define a `TodoListCtrl` controller that places injected
+// `todos` (from a UI-Router state resolve) on the scope.
+// It should have a scope method `setCategory` that changes
+// `$scope.filterByCompleted` to certain values; a method
+// `isActiveCategory` that confirms the current category;
+// and a method `addTodo` that saves `$scope.toAdd` to the backend,
+// then goes to that new todo's edit state. (If you are confused about
+// where `$scope.toAdd` is coming from, check out the `todo.list.html`.)
