5d8368b850f6afdf8c944dc95e24019a15351686
TheBeff <thebeff@gmail.com>
2016-10-04 20:08:17 -0400

    Finished Todos detail

--- browser/app/todos/detail/todo.detail.state.js
+++ browser/app/todos/detail/todo.detail.state.js
@@ -2,3 +2,18 @@
 // based on the id in the url used to reach the state.
 // Note: it isn't tested, but you should also wire up the
 // associated controller and templateUrl for a functioning app.
+
+app.config(function($stateProvider){
+
+	$stateProvider
+	  .state('todos.detail', {
+	  	url: '/:id',
+	  	templateUrl: './todo.detail.html',
+	  	resolve: {
+	  		todo: function(Todo, $stateParams){
+	  			return Todo.getOne($stateParams.id);
+	  		}
+	  	}
+	  });
+
+});
--- browser/app/todos/list/todo.list.state.js
+++ browser/app/todos/list/todo.list.state.js
@@ -1,4 +1,4 @@
-// define a `todos` state which resolves all `todos`.
+ // define a `todos` state which resolves all `todos`.
 // note: it isn't tested, but you should also wire up the
 // associated controller and templateUrl for a functioning app.
 
